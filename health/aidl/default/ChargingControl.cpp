47e1d98c6f2e089728421539649243f50fdb0973
LordShenron <shen.priyanshu@gmail.com>
2025-03-01 05:00:19 +0100

    Health: remove for now
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>

--- health/aidl/default/ChargingControl.cpp
+++ health/aidl/default/ChargingControl.cpp
@@ -22,7 +22,7 @@ namespace health {
 
 #ifdef HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE
 static const std::vector<ChargingEnabledNode> kChargingEnabledNodes = {
-        {HEALTH_CHARGING_CONTROL_CHARGING_PATH, HEALTH_CHARGING_CONTROL_CHARGING_ENABLED,
+        {HEALTH_CHARGING_CONTROL_CHARGING_ENABLED,
          HEALTH_CHARGING_CONTROL_CHARGING_DISABLED},
         {"/sys/class/power_supply/battery/battery_charging_enabled", "1", "0"},
         {"/sys/class/power_supply/battery/charging_enabled", "1", "0"},
